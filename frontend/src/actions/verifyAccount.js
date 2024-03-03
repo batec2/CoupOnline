a6bc4ebebd773d4fba6d7bb51103a02e983d52d5
Neal <nealhamacher@gmail.com>
2024-03-02 18:32:27 -0700

    Login page takes to room if login successful

--- frontend/src/actions/verifyAccount.js
+++ frontend/src/actions/verifyAccount.js
@@ -1,10 +1,13 @@
 import { terminal } from 'virtual:terminal'
-import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 
 const verifyAccount = async (username, setVerified) => {
-    const navigate = useNavigate();
-    navigate("/room");
-    terminal.log("Clicked on verify!");    
+  const response = await axios.get(`http://localhost:8080/players/byName/${username}`)
+  if(response.status == 200) {
+    return true
+  }
+  return false
 }
 
 export default verifyAccount;
--- frontend/src/pages/login/login.page.jsx
+++ frontend/src/pages/login/login.page.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { terminal } from 'virtual:terminal'
 
 import TextField from "../../components/textfield/textfield.component";
 import verifyAccount from "../../actions/verifyAccount";
@@ -12,22 +13,30 @@ const LoginPage = () => {
     const navigate = useNavigate();
 
     const[username, setUsername] = useState([]);
-    const[verified, setVerified] = useState(false)
+    const[verified, setVerified] = useState(false);
 
     const handleUsernameInput = e => {
       setUsername(e.target.value);
-    }
+    };
 
     const handleCreateClick = () => {
       navigate("/creation")
-    }
+    };
+
+    const handleLoginClick = async () => {
+      const verif = await verifyAccount(username, setVerified);
+      terminal.log(verif)
+      if (verif) {
+        navigate("/room");
+      };
+    };
 
     return (
       <div>
         <h1>Welcome to Super Couper</h1>
         <h2>Enter your username:</h2>
         <TextField placeholder="Username" onChange={handleUsernameInput}/>
-        <button onClick={verifyAccount(username, setVerified)}>Login</button>
+        <button onClick={handleLoginClick}>Login</button>
         <h2>No account?</h2>
         <button onClick={handleCreateClick}>Create an account</button>
       </div>
