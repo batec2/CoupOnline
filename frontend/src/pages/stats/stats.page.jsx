dfbb937f420ecd17b0f79a965970df20b4b4817f
Conlan2 <conlan2222@gmail.com>
2024-03-04 22:56:53 -0700

    Added a page for displaying the stats of users, in progress

--- frontend/src/pages/stats/stats.page.jsx
+++ frontend/src/pages/stats/stats.page.jsx
@@ -1,12 +1,50 @@
+import {useQuery, useQueryClient} from "@tanstack/react-query";
+import {useState} from "react";
+import axios from "axios";
+import StatsTable from "../../components/statsTable/statsTable.component.jsx";
+
+
+const axiosClientPlayers = axios.create({
+    baseURL: "http://localhost:8080",
+    headers: {
+            Accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Access-Control-Allow-Origin": "*"}
+})
+
 /**
  * Page for getting the global stats, for example
  * win-rate per card, most winning card combinations, ect..
  * @returns Stats/Leader Board
  */
 const StatsPage = () => {
+
+  const queryClient = useQueryClient()
+  const [AllPlayers, setAllPlayers] = useState([])
+
+  const {
+    data: results,
+    isError,
+    isSuccess
+  } = useQuery({
+                queryKey: ["players"],
+                queryFn: () => {axiosClientPlayers.get('/players').then((res) => {
+                  console.log(res.data)
+                  return res.data
+                  })}})
+
+  console.log(results)
+
+
   return (
     <div>
-      <h1>Stats Page</h1>
+      {isSuccess &&
+        <StatsTable entries={AllPlayers}></StatsTable>
+      }
+      {
+       isError &&
+       <h3>Error Occurred In Retrieving Player Stats</h3>
+      }
     </div>
   );
 };
