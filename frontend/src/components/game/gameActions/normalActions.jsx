d78ff8c4736e8eadacefc98e1e11308d83153a32
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-03-22 20:21:10 -0600

    Disabled onClick on action buttons when action is unavailable

--- frontend/src/components/game/gameActions/normalActions.jsx
+++ frontend/src/components/game/gameActions/normalActions.jsx
@@ -11,59 +11,58 @@ const NormalActions = () => {
   const [showTarget, setShowTarget] = useState(false);
   const [currentAction, setAction] = useState(null);
 
-  const mustCoup = () => {
-    return coins >= 10;
-  }
+  const normal = "bg-actions-normal";
+  const unavailable = "bg-actions-unavailable";
+  const haveCard = "bg-actions-haveCard";
+  const bluff = "bg-actions-bluff";
+
   const buttonColor = (button) => {
     if(coins >= 10 && button != GameActions.Coup) {
-      return "bg-actions-unavailable";
+      return unavailable;
     }
     switch(button){
       case GameActions.Coup:{
          if(coins > 6) {
-          return "bg-actions-normal";
-         }
-         else {
-          return "bg-actions-unavailable";
+          return normal;
+         } else {
+          return unavailable;
          }
       }
       case GameActions.Income || GameActions.Aid:{
-        return "bg-actions-normal";
+        return normal;
       }
       case GameActions.Taxes: {
         if(gameCards[0] == GameCard.Duke || gameCards[1] == GameCard.Duke) {
-          return "bg-actions-haveCard"
-        }
-        else {
-          return "bg-actions-bluff";
+          return haveCard;
+        } else {
+          return bluff;
         }
       }
       case GameActions.Exchange: {
-        if(gameCards[0] == GameCard.Ambassador || gameCards[1] == GameCard.Ambassador) {
-          return "bg-actions-haveCard"
-        }
-        else {
-          return "bg-actions-bluff";
+        if(gameCards[0] == GameCard.Ambassador 
+           || gameCards[1] == GameCard.Ambassador) {
+          return haveCard;
+        } else {
+          return bluff;
         }
       }
       case GameActions.Assassinate: {
         if(coins < 3) {
-          return "bg-actions-unavailable"
-        }
-        else if(gameCards[0] == GameCard.Assassin || gameCards[1] == GameCard.Assassin) {
-          return "bg-actions-haveCard"
-        }
-        else {
-          return "bg-actions-bluff";
+          return unavailable;
+        } else if(gameCards[0] == GameCard.Assassin 
+                  || gameCards[1] == GameCard.Assassin) {
+          return haveCard;
+        } else {
+          return bluff;
         }
       }
       
       case GameActions.Steal: {
-        if(gameCards[0] == GameCard.Captain || gameCards[1] == GameCard.Captain) {
-          return "bg-actions-haveCard"
-        }
-        else {
-          return "bg-actions-bluff";
+        if(gameCards[0] == GameCard.Captain 
+           || gameCards[1] == GameCard.Captain) {
+          return haveCard;
+        } else {
+          return bluff;
         }
       }
     }
@@ -75,19 +74,21 @@ const NormalActions = () => {
         <Button
           className={buttonColor(GameActions.Income)}
           onClick={() => {
-            handleNormalAction(socket, roomId, GameActions.Income);
-            setShowTarget(false);
-            setAction(GameActions.Income);
+            if(buttonColor(GameActions.Income) != unavailable) {
+              handleNormalAction(socket, roomId, GameActions.Income);
+              setShowTarget(false);
+              setAction(GameActions.Income);
+            }
           }}
         >
           Income
         </Button>
 
         <Button
-          className={coins>6 ? "bg-actions-normal" : "bg-actions-unavailable"}
+          className={buttonColor(GameActions.Coup)}
           onClick={() => {
             // handleNormalAction(socket, roomId, GameActions.Coup);
-            if (coins > 6) {
+            if (buttonColor(GameActions.Coup) != {unavailable}) {
               setShowTarget(true);
               setAction(GameActions.Coup);
             }
@@ -101,8 +102,10 @@ const NormalActions = () => {
         <Button
           className={buttonColor(GameActions.Taxes)}
           onClick={() => {
-            handleNormalAction(socket, roomId, GameActions.Taxes);
-            setAction(GameActions.Taxes);
+            if (buttonColor(GameActions.Taxes) != unavailable) {
+              handleNormalAction(socket, roomId, GameActions.Taxes);
+              setAction(GameActions.Taxes);
+            }
           }}
         >
           Taxes
@@ -111,9 +114,11 @@ const NormalActions = () => {
         <Button
           className={buttonColor(GameActions.Exchange)}
           onClick={() => {
-            handleNormalAction(socket, roomId, GameActions.Exchange);
-            setShowTarget(false);
-            setAction(GameActions.Exchange);
+            if(buttonColor(GameActions.Exchange) != unavailable) {
+              handleNormalAction(socket, roomId, GameActions.Exchange);
+              setShowTarget(false);
+              setAction(GameActions.Exchange);
+            }
           }}
         >
           Exchange Influence
@@ -123,9 +128,11 @@ const NormalActions = () => {
         <Button
           className={buttonColor(GameActions.Aid)}
           onClick={() => {
-            handleNormalAction(socket, roomId, GameActions.Aid);
-            setShowTarget(false);
-            setAction(GameActions.Aid);
+            if(buttonColor(GameActions.Aid) != unavailable) {
+              handleNormalAction(socket, roomId, GameActions.Aid);
+              setShowTarget(false);
+              setAction(GameActions.Aid);
+            }
           }}
         >
           Foreign Aid
@@ -134,9 +141,11 @@ const NormalActions = () => {
         <Button
           className={buttonColor(GameActions.Assassinate)}
           onClick={() => {
-            handleNormalAction(socket, roomId, GameActions.Assassinate);
-            setShowTarget(true);
-            setAction(GameActions.Assassinate);
+            if(buttonColor(GameActions.Assassinate) != unavailable) {
+              handleNormalAction(socket, roomId, GameActions.Assassinate);
+              setShowTarget(true);
+              setAction(GameActions.Assassinate);
+            }
           }}
         >
           Assassinate
@@ -144,9 +153,11 @@ const NormalActions = () => {
         <Button
           className={buttonColor(GameActions.Steal)}
           onClick={() => {
-            handleNormalAction(socket, roomId, GameActions.Steal);
-            setShowTarget(true);
-            setAction(GameActions.Steal);
+            if(buttonColor(GameActions.Steal) != unavailable) {
+              handleNormalAction(socket, roomId, GameActions.Steal);
+              setShowTarget(true);
+              setAction(GameActions.Steal);
+            }
           }}
         >
           Steal
