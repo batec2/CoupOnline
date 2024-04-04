414a2723a321545994dcb5a4f82d8fcb98df6085
Crush Bate <50185023+batec2@users.noreply.github.com>
2024-04-04 11:45:33 -0600

    changed mongo session url

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -2,19 +2,19 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 //import cookieParser from "cookie-parser";
-import session from "express-session"
-import { default as connectMongoDBSession} from "connect-mongodb-session"
+import session from "express-session";
+import { default as connectMongoDBSession } from "connect-mongodb-session";
 
-const SESS_SECRET = "test"
-const COOKIE_NAME = "AxiosCookie"
-const MAX_AGE = 1000 * 60 * 60 * 3
-const MongoURI = "mongodb://localhost:27017/couponline"
+const SESS_SECRET = "test";
+const COOKIE_NAME = "AxiosCookie";
+const MAX_AGE = 1000 * 60 * 60 * 3;
+const MongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bxbwsjp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-const MongoDBStore = connectMongoDBSession(session)
+const MongoDBStore = connectMongoDBSession(session);
 
 const mongoDBstore = new MongoDBStore({
   uri: MongoURI,
-  collection: "mySessions"
+  collection: "mySessions",
 });
 
 const app = express();
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-    credentials: true
+    credentials: true,
   })
 );
 
@@ -31,7 +31,8 @@ app.use(morgan("dev")); //console logging
 app.use(express.json()); //body parsing
 app.use(express.urlencoded({ extended: true })); //query string
 
-app.use(session({
+app.use(
+  session({
     name: COOKIE_NAME,
     secret: SESS_SECRET,
     resave: false,
@@ -40,9 +41,10 @@ app.use(session({
     cookie: {
       maxAge: MAX_AGE,
       sameSite: false,
-      secure: false
-    }
-  }))
+      secure: false,
+    },
+  })
+);
 // app.use(cookieParser());
 
 export default app;
