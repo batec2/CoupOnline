8a8a0c72efe7101cc739faa257c5691ee692c7b9
Crush Bate <50185023+batec2@users.noreply.github.com>
2024-04-05 03:58:59 -0600

    remove slash from origin

--- backend/event-handlers/IOEvents.js
+++ backend/event-handlers/IOEvents.js
@@ -1,7 +1,7 @@
 import { Server } from "socket.io";
 export const createSocketIO = (httpServer, rooms) => {
   const io = new Server(httpServer, {
-    cors: { origin: "http://localhost:5173/", methods: ["GET", "POST"] },
+    cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] },
   });
   /**
    * Listens for room deletion event, event is triggered when room is empty
