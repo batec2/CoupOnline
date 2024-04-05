f8f8df3b2d154f2d822d74aff48d958fa593eb65
Crush Bate <50185023+batec2@users.noreply.github.com>
2024-04-05 02:59:04 -0600

    change socket.id to cookie ref for player name

--- frontend/src/components/game/game.component.jsx
+++ frontend/src/components/game/game.component.jsx
@@ -5,44 +5,42 @@ import DiscardList from "../cardList/discardList.component";
 import References from "./references/references.component";
 import PlayerInfo from "./playerInfo/playerInfo.component";
 import TurnHistory from "./turnHistory/turnHistory.component";
-import terminal from "virtual:terminal"
+import terminal from "virtual:terminal";
 
 import "./game.styles.css";
 
-
-
 const Game = () => {
-  const { roomId, currentLobbyMembers, socket } = useGameContext();
-  const player = currentLobbyMembers[socket.current.id].userId
+  const { roomId, currentLobbyMembers, cookieRef, socket } = useGameContext();
+  const player = cookieRef.current.id;
 
   return (
-    <div className="flex justify-center">    
-    <div className="h-screen max-w-screen-2xl">
-      <div className="flex-row inline-flex w-full justify-between text-center text-xl font-bold">
-        <p>Player: {player}</p>
-        <p>Room: {roomId}</p> 
+    <div className="flex justify-center">
+      <div className="h-screen max-w-screen-2xl">
+        <div className="flex-row inline-flex w-full justify-between text-center text-xl font-bold">
+          <p>Player: {player}</p>
+          <p>Room: {roomId}</p>
+        </div>
+        <div className="max-w-sm:flex max-w-sm:flex-col sm:grid sm:grid-cols-4">
+          <div className="max-w-sm:order-1 sm:order-2 border sm:col-span-2 game-panels">
+            <CurrentTurnInfo />
+          </div>
+          <div className="max-w-sm:order-2 sm:order-5 min-h-full sm:col-span-2 game-panels">
+            <PlayerInfo />
+          </div>
+          <div className="max-w-sm:order-3 sm:order-1 game-panels">
+            <PlayerCardList />
+          </div>
+          <div className="max-w-sm:order-4 sm:order-4 game-panels h-0 min-h-full overflow-y-auto">
+            <TurnHistory />
+          </div>
+          <div className="max-w-sm:order-5 sm:order-3 game-panels">
+            <DiscardList />
+          </div>
+          <div className="max-w-sm:order-6 sm:order-6 min-h-full game-panels">
+            <References />
+          </div>
+        </div>
       </div>
-      <div className="max-w-sm:flex max-w-sm:flex-col sm:grid sm:grid-cols-4">
-        <div className="max-w-sm:order-1 sm:order-2 border sm:col-span-2 game-panels">
-          <CurrentTurnInfo />
-        </div>
-        <div className="max-w-sm:order-2 sm:order-5 min-h-full sm:col-span-2 game-panels">
-          <PlayerInfo />
-        </div>
-        <div className="max-w-sm:order-3 sm:order-1 game-panels">
-          <PlayerCardList />
-        </div>
-        <div className="max-w-sm:order-4 sm:order-4 game-panels h-0 min-h-full overflow-y-auto">
-          <TurnHistory />
-        </div>
-        <div className="max-w-sm:order-5 sm:order-3 game-panels">
-          <DiscardList />
-        </div>
-        <div className="max-w-sm:order-6 sm:order-6 min-h-full game-panels">
-          <References />
-        </div>
-      </div>
-    </div>
     </div>
   );
 };
