4b1d22c119e7c85849d02dbbbda89c2bfe8775d4
Crush Bate <50185023+batec2@users.noreply.github.com>
2024-04-05 03:13:04 -0600

    convert to use .env for server

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -8,9 +8,10 @@ import { default as connectMongoDBSession } from "connect-mongodb-session";
 const SESS_SECRET = "test";
 const COOKIE_NAME = "AxiosCookie";
 const MAX_AGE = 1000 * 60 * 60 * 3;
-// const MongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bxbwsjp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-const MongoURI = `mongodb://127.0.0.1:27017/couponline`;
+const MongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+// const MongoURI = `mongodb://127.0.0.1:27017/couponline`;
 
 const MongoDBStore = connectMongoDBSession(session);
 
@@ -21,14 +22,7 @@ const mongoDBstore = new MongoDBStore({
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-
+app.use(cors());
 app.use(morgan("dev")); //console logging
 app.use(express.json()); //body parsing
 app.use(express.urlencoded({ extended: true })); //query string
